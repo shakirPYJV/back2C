9b3e07b3e15499733592f26e477e5fc8b61037fb
SHakirPY <101662958+shakirPYJV@users.noreply.github.com>
2024-03-11 19:54:42 +0600

    basic_calculator.c

---
+++ basic_calculator.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+{
+	char operator;
+	double num_1,num_2,result;
+	printf ("Enter the operator + - * / : ");
+	scanf ("%c",&operator);
+	
+if (operator == '+'|| operator == '-'|| operator == '*'|| operator == '/'){
+
+	printf("Enter the first number: ");
+	if(scanf("%lf",&num_1) != 1){
+		printf("Invalid Input");
+		return 1;
+	}
+	printf("Enter the second number: ");
+	if(scanf("%lf",&num_2) != 1){
+		printf("Invalid Input");
+		return 1;
+	}
+
+	switch(operator){
+
+	case '+':
+		result = num_1 + num_2;
+		printf("The result is: %.2lf",result);
+		break;
+	case '-':
+		result = num_1 - num_2;
+		printf("The result is: %.2lf",result);
+		break;
+	case '*':
+		result = num_1 * num_2;
+		printf("The result is: %.2lf",result);
+		break;
+	case '/':
+		if (num_2 == 0){
+			printf("You cant divide by 0!");
+			return 1;
+		}
+		result = num_1 / num_2;
+		printf("The result is: %.2lf",result);
+		break;
+	default:
+		printf("Error!\n");
+	}
+}
+
+else{
+	printf("\nPlease enter a valid operator!");
+}
+	return 0;
+
+}
+
